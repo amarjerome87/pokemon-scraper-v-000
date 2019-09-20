e70a361b0fd82f142cceee11aefa9a49d2fd7fdd
Amar Chatman <amarjerome@gmail.com>
2019-09-20 04:08:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,11 @@ end
 def self.find (id,db)
   #binding.pry
   data = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+<<<<<<< HEAD
 
+=======
+  #binding.pry
+>>>>>>> 29b44a938d7211a6ca54f2231701a559145417ee
 Pokemon.new(id: data[0][0], name: data[0][1], type: data[0][2], db: db)
 
 end
